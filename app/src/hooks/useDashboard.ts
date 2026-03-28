71a4e2e7724fa29c6b7d1b816075b2c04a5b3417
Armando Vazquez <armancma777@gmail.com>
2026-03-28 11:48:33 -0600

    Configuración de Dashboard

--- app/src/hooks/useDashboard.ts
+++ app/src/hooks/useDashboard.ts
@@ -4,4 +4,12 @@ import useFetchPro from "./useFetchPro";
 const useDashboard=()=>{
     const crypto= useCryptoPrices();
     const users = useFetchPro("https://jsonplaceholder.typicode.com/users");
+
+    return{
+        loading: crypto.loading || users.loading,
+        data:{
+            crypto: crypto.data,
+            users: users.data
+        }
+    }
 }

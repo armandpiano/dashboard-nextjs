1334e19adc7d5e9f5a73b5f3fc0e357f0deb9583
Armando Vazquez <armancma777@gmail.com>
2026-03-28 10:15:27 -0600

    Configuracion de Arquitectura Atomica

---
+++ app/src/hooks/useCrypto.ts
@@ -0,0 +1,7 @@
+
+
+//use porque es Hook
+//useFetch (extraer recursos de una API)
+const useCryptoPrices=()=>{
+
+}
---
+++ app/src/hooks/useFetchPro.ts
@@ -0,0 +1,5 @@
+import { useState,useEffect,useRef } from "react";
+
+type Options={
+    
+}
